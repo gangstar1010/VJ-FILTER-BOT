cf769a7eb32a68057750131df7ebaaa4b396c511
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2024-08-15 23:42:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -583,7 +583,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = '@VJ_Bots  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
+    title = '@SH_OTT  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
     size=get_size(files.file_size)
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
